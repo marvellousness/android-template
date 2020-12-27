449e5f8926e21c8bf7d43268c6d459c6b51fb9a0
Phan Thanh Tung <tungphan@asnet.com.vn>
2020-12-27 21:30:57 +0700

    First commit

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,14 @@
+object AppConfig {
+    const val APP_ID = "com.jt.android.template"
+    const val APP_VERSION_NAME = "1.0.0"
+    const val APP_VERSION_CODE = 1
+}
+
+object LibraryAndroidCoordinates {
+    const val LIBRARY_VERSION = "1.0"
+    const val LIBRARY_VERSION_CODE = 1
+}
+
+object LibraryKotlinCoordinates {
+    const val LIBRARY_VERSION = "1.0"
+}
